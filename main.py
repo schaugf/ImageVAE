4d27ced59f377b7c7c6bf64ec7d5ad7373086638
schaugf <schau.geoffrey@gmail.com>
2018-09-12 20:38:52 +0000

    rewrite img_dir if None provided

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ parser.add_argument('--learn_rate',     type=float, default=0.001,      help='le
 parser.add_argument('--epsilon_std',    type=float, default=1.0,        help='epsilon width')
 parser.add_argument('--latent_samp',    type=int,   default=10,         help='vaecb: number of latent samples')
 parser.add_argument('--num_save',       type=int,   default=8,          help='vaecb: number of reconstructed images to save')
+parser.add_argument('--do_tsne', 		type=int,   default=1,          help='run tsne analysis? 1=yes')
 parser.add_argument('--verbose',        type=int,   default=2,          help='1=verbose, 2=quiet')
 parser.add_argument('--steps_per_epoch',    type=int,   default=0,      help='steps per epoch')
 args = parser.parse_args()
@@ -38,6 +39,9 @@ def main():
 
     os.makedirs(args.save_dir, exist_ok=True)
     os.makedirs(os.path.join(args.save_dir, 'checkpoints'), exist_ok=True)
+
+    if args.image_dir == None:
+        args.image_dir = os.path.join(args.data_dir, 'train')
 
     if args.phase == 'train':
         model = ImageVAE(args)
